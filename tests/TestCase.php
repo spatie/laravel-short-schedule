b40f91fc1a43fce3e57e2ab576186806f2e792fe
Freek Van der Herten <freek@spatie.be>
2020-06-05 12:25:22 +0200

    Initial commit

---
+++ tests/TestCase.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Spatie\Skeleton\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use Spatie\Skeleton\SkeletonServiceProvider;
+
+class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withFactories(__DIR__.'/database/factories');
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            SkeletonServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+
+        /*
+        include_once __DIR__.'/../database/migrations/create_skeleton_tables.php.stub';
+        (new CreatePackageTables())->up();
+        */
+    }
+}
