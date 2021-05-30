5a9483ab2b9fb6c975d204781720d6588821ef70
Jakongir91 <Jakhongir>
2021-05-30 19:06:50 +0500

    Rename console command's option name

--- src/Commands/ShortScheduleRunCommand.php
+++ src/Commands/ShortScheduleRunCommand.php
@@ -8,15 +8,15 @@ use Spatie\ShortSchedule\ShortSchedule;
 
 class ShortScheduleRunCommand extends Command
 {
-    protected $signature = 'short-schedule:run {--life-time=}';
+    protected $signature = 'short-schedule:run {--lifetime=}';
 
     protected $description = 'Run the short scheduled commands
-                              {--life-time= : The life time of worker';
+                              {--lifetime= : The life time of worker';
 
     public function handle()
     {
         $loop = Factory::create();
 
-        (new ShortSchedule($loop))->registerCommands()->run($this->option('life-time'));
+        (new ShortSchedule($loop))->registerCommands()->run($this->option('lifetime'));
     }
 }
