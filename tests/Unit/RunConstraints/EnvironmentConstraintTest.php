5dc5dfcd515cb3e26b2758b3b42d99d6b6f50d8c
Alex Manase <malexandru699@gmail.com>
2023-02-06 20:57:26 +0200

    fix: failing test

--- tests/Unit/RunConstraints/EnvironmentConstraintTest.php
+++ tests/Unit/RunConstraints/EnvironmentConstraintTest.php
@@ -2,6 +2,8 @@
 
 use Spatie\ShortSchedule\RunConstraints\EnvironmentConstraint;
 
+uses(Spatie\ShortSchedule\Tests\TestCase::class);
+
 it('will constraint execution based on the environment', function () {
     $constraint = new EnvironmentConstraint(['local', 'production']);
     expect($constraint->shouldRun())->toBeFalse();
