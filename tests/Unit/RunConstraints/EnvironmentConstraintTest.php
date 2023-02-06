6bc91e56db40fa17158a801fbd614079827f4296
Alex Manase <malexandru699@gmail.com>
2023-02-06 20:54:00 +0200

    refactor: EnvironmentConstraintTest

--- tests/Unit/RunConstraints/EnvironmentConstraintTest.php
+++ tests/Unit/RunConstraints/EnvironmentConstraintTest.php
@@ -1,19 +1,11 @@
 <?php
 
-namespace Spatie\ShortSchedule\Tests\Unit\RunConstraints;
-
 use Spatie\ShortSchedule\RunConstraints\EnvironmentConstraint;
-use Spatie\ShortSchedule\Tests\TestCase;
 
-class EnvironmentConstraintTest extends TestCase
-{
-    /** @test */
-    public function it_will_constraint_execution_based_on_the_environment()
-    {
-        $constraint = new EnvironmentConstraint(['local', 'production']);
-        $this->assertFalse($constraint->shouldRun());
+it('will constraint execution based on the environment', function () {
+    $constraint = new EnvironmentConstraint(['local', 'production']);
+    expect($constraint->shouldRun())->toBeFalse();
 
-        $constraint = new EnvironmentConstraint(['local', 'production', 'testing']);
-        $this->assertTrue($constraint->shouldRun());
-    }
-}
+    $constraint = new EnvironmentConstraint(['local', 'production', 'testing']);
+    expect($constraint->shouldRun())->toBeTrue();
+});
