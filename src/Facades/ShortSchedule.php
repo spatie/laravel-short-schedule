ad845120b863767636b29fab2cd7b44e6c9673b5
Andrew Riddlestone <andrew.riddlestone@gmail.com>
2024-08-15 16:10:15 +0100

    Added facade class docblock with static methods

--- src/Facades/ShortSchedule.php
+++ src/Facades/ShortSchedule.php
@@ -4,11 +4,15 @@ namespace Spatie\ShortSchedule\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static useLoop(\React\EventLoop\LoopInterface $loop): \Spatie\ShortSchedule\ShortSchedule
+ * @method static command(string $command): \Spatie\ShortSchedule\PendingShortScheduleCommand
+ * @method static exec(string $command): \Spatie\ShortSchedule\PendingShortScheduleCommand
+ * @method static registerCommandsFromConsoleKernel(): \Spatie\ShortSchedule\ShortSchedule
+ * @method static run(?int $lifetime = null): void
+ */
 class ShortSchedule extends Facade
 {
-    /**
-     * @mixin \Spatie\ShortSchedule\ShortSchedule
-     */
     public static function getFacadeAccessor()
     {
         return \Spatie\ShortSchedule\ShortSchedule::class;
